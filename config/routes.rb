0a0b44a7922ca8c3f2394350c310a0593c60541c
Vlad <vladdi.rozzi@gmail.com>
2017-09-26 13:30:30 +0300

    add a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
